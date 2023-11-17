5e691f5a891d8cc2f8a6b454e89cc4feecf774ca
Stepan Bogdanov <bogstepan@gmail.com>
2023-11-17 13:57:33 +0300

    1. RESTFul. Описание архитектуры [#6884 #459829]

--- src/main/java/ru/job4j/auth/controller/PersonController.java
+++ src/main/java/ru/job4j/auth/controller/PersonController.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import ru.job4j.auth.domain.Person;
 import ru.job4j.auth.repository.PersonRepository;
 import ru.job4j.auth.service.PersonService;
@@ -25,22 +26,36 @@ public class PersonController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Person> findById(@PathVariable int id) {
-        return personService.findById(id);
+        var person = personService.findById(id);
+        return new ResponseEntity<Person>(
+                person.orElse(new Person()),
+                person.isPresent() ? HttpStatus.OK : HttpStatus.NOT_FOUND
+        );
     }
 
     @PostMapping("/")
     public ResponseEntity<Person> create(@RequestBody Person person) {
-        return personService.create(person);
+        var savedPerson = personService.create(person);
+        return new ResponseEntity<Person>(
+                savedPerson.orElse(new Person()),
+                savedPerson.isPresent() ? HttpStatus.CREATED : HttpStatus.CONFLICT
+        );
     }
 
     @PutMapping("/")
     public ResponseEntity<Void> update(@RequestBody Person person) {
-        return personService.update(person);
+        if (!personService.update(person)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "A person with this ID has not been found");
+        }
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable int id) {
-        return personService.delete(id);
+        if (!personService.delete(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "A person with this ID has not been found");
+        }
+        return ResponseEntity.ok().build();
     }
 }
 
