c17d809adf2514eabb1c0d2e1e3981d290028e8e
Stepan Bogdanov <bogstepan@gmail.com>
2023-11-29 20:36:50 +0300

    1. Spring Liquibase [#302330]

--- src/main/java/ru/job4j/auth/AuthApplication.java
+++ src/main/java/ru/job4j/auth/AuthApplication.java
@@ -1,9 +1,12 @@
 package ru.job4j.auth;
 
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class AuthApplication {
@@ -16,6 +19,15 @@ public class AuthApplication {
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+
+	@Bean
+	public SpringLiquibase liquibase(DataSource ds) {
+		SpringLiquibase liquibase = new SpringLiquibase();
+		liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+		liquibase.setDataSource(ds);
+		return liquibase;
+	}
+
 
 
 }
